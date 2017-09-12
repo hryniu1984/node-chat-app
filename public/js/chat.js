3bbc95de307bfaf42f507f0f7c69129baf707fd8
Piotr Hryniewicz <piotr.hryniewicz84@gmail.com>
2017-09-12 03:22:19 +0200

    Add Users class and updateUserList event

--- public/js/chat.js
+++ public/js/chat.js
@@ -15,7 +15,15 @@ function scrollToBottom () {
 }
 
 socket.on('connect', function () {
-  console.log('Connected to server');
+  var params = jQuery.deparam(window.location.search);
+  socket.emit('join', params, function(err) {
+  	if(err) {
+  		alert(err);
+  		window.location.href = '/';
+  	} else {
+  		console.log('No error');
+  	}
+  });
 });
 
 socket.on('disconnect', function () {
@@ -38,6 +46,16 @@ socket.on('newMessage', function (message) {
   // li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
   // jQuery('#messages').append(li);
+});
+
+socket.on('updateUserList', function(users) {
+  var ol = jQuery('<ol></ol>');
+
+  users.forEach(function(user) {
+    ol.append(jQuery('<li></li>').text(user));
+  });
+
+  jQuery('#users').html(ol);
 });
 
 socket.on('newLocationMessage', function (message) {
